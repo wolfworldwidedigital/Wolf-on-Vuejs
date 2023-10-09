b21c4ceb2e4d5a6bbac2517c916ab9cd25f5b427
David | Wolf Worldwide <88748661+wolfworldwidedigital@users.noreply.github.com>
2023-10-09 20:37:37 +0530

    Delete components/AboutUs directory

--- components/AboutUs/components/Blogs/Details/Image.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="img">
-    <img src="/img/blog/single.jpg" alt="" />
-  </div>
-</template>
--- components/AboutUs/components/Demos/CircleBg.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="circle-bg">
-    <div class="circle-color fixed">
-      <div class="gradient-circle"></div>
-      <div class="gradient-circle two"></div>
-    </div>
-  </div>
-</template>
